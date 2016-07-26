433eb6daae4d99c1affa9b96e721e92cd109056c
n"natalia-kochanska <wczorajszy_zgon@wp.pl>
2016-07-26 11:54:50 +0800

    added mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,9 +1,10 @@
 class ApplicationMailer < ActionMailer::Base
   default from: "reservarion@ourbnb.com"
   
-  def reservation_email(customer, host, reservation_id)
-  	@reservarion.user = customer
-  	@listing.user = host
-  	@reservation.id = reservation_id
+  def reservation_email(customer, host, listing)
+  	@customer = customer
+  	@host = host  	
+  	@listing = listing
+  	mail(to: @host.email, subject: 'New reservation for your listing')
   end
 end
